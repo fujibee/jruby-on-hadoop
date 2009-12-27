d754b7a227356f8c9582c4fdb15539972eec6bbb
Koichi Fujikawa <fujibee@gmail.com>
2009-12-28 00:51:05 +0900

    hadoop client and refactoring of changing filenames, etc.

--- lib/jruby_on_hadoop.rb
+++
@@ -1,7 +0,0 @@
-require 'jruby_on_hadoop/hadoop_client'
-
-module JrubyOnHadoop
-  def self.jar_path
-    File.join(File.expand_path(File.dirname(__FILE__), "hadoop-ruby.jar"))
-  end
-end
--- spec/jruby_on_hadoop_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'jruby_on_hadoop'
-
-describe JrubyOnHadoop do
-  it 'should return jar path' do
-    jar_dir = File.join(File.dirname(__FILE__), '..', 'lib')
-    jar_path = File.join(File.expand_path(jar_dir), 'hadoop-ruby.jar')
-    JrubyOnHadoop.jar_path.should == jar_path
-  end
-end
